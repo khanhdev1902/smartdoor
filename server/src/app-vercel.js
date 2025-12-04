edd5694ce0f95d9c594e69be24a42ee3a478482c
Văn Khanh <khanhdev1902@gmail.com>
2025-12-04 13:36:18 +0700

    update vercel

---
+++ server/src/app-vercel.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const { userRouter } = require("./routers/UserRouter");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter);
+
+module.exports = app;
